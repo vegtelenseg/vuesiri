f888517851f5e4aa871d6f38ef7022fd0e6d0e1b
Siya <dot.netntoz@gmail.com>
2018-07-08 12:01:33 +0200

    Added header and carousel section

---
+++ src/components/carousel/carousel.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Slider from 'react-slick';
+import pic1 from '../../assets/images/carousel/frankie-min.jpg';
+import pic2 from '../../assets/images/carousel/alex-holyoake-min.jpg';
+import pic3 from '../../assets/images/carousel/smoke-min.jpg';
+import pic4 from '../../assets/images/carousel/jay-wennington-min.jpg';
+
+export default class Carousel extends React.Component {
+    render() {
+        var settings = {
+            dots: true,
+            arrows: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        return (
+            <Slider {...settings}>
+                <div>
+                    <img src={pic1} className="carousel-item" />
+                </div>
+                <div>
+                    <img src={pic2} className="carousel-item" />
+                </div>
+                <div>
+                    <img src={pic3} className="carousel-item" />
+                </div>
+                <div>
+                    <img src={pic4} className="carousel-item" />
+                </div>
+            </Slider>
+        );
+    }
+}
