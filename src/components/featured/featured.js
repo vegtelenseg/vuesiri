ce4124e72a48662577b17e1d816843e646e65b52
Siya <dot.netntoz@gmail.com>
2018-07-08 18:10:44 +0200

    Added featured artists and styling

--- src/components/featured/featured.js
+++ src/components/featured/featured.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+const images = [
+    {
+        url: require('../../assets/images/featured/nathan.jpg'),
+        alt: 'nathan'
+    },
+    {
+        url: require('../../assets/images/featured/oladimeji.jpg'),
+        alt: 'oladimeji'
+    },
+    {
+        url: require('../../assets/images/featured/tanja.jpg'),
+        alt: 'tanja'
+    },
+    {
+        url: require('../../assets/images/featured/tyler.jpg'),
+        alt: 'tyler'
+    }
+];
 export default class Featured extends React.Component {
     render() {
         return (
             <div>
                 <p className="featured-heading">Featured Artists</p>
+                <div className="featured">
+                    {images.map((img, idx) => (
+                        <img key={idx} alt={img.alt} src={img.url} className="" />
+                    ))}
+                </div>
             </div>
         );
     }
