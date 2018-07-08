9eb7479c3d1e1dd913af701a153b62f74f884ac4
Siya <dot.netntoz@gmail.com>
2018-07-08 17:14:13 +0200

    Added the featured component

---
+++ src/components/featured/featured.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Featured extends React.Component {
+    render() {
+        return (
+            <div>
+                <p className="featured-heading">Featured Artists</p>
+            </div>
+        );
+    }
+}
