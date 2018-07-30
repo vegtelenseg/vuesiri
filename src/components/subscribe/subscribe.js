e0466c04d3da0e2b22c3b65245fa95a50548c262
végtelenség <dot.netntoz@gmail.com>
2018-07-30 13:35:10 +0200

    Added fonts and styles

--- src/components/subscribe/subscribe.js
+++ src/components/subscribe/subscribe.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import subBanner from '../../assets/images/subscribe/subscribe.jpg';
+import iPhone from '../../assets/images/subscribe/iPhone.png';
 export default class Subscribe extends React.Component {
     render() {
         return (
             <div className="subscribe">
-                <img src={subBanner} />
+                <img src={subBanner}   alt="vuesiri-banner"/>
+                <div className="iPhone-container">
+                    <img className="iPhone" src={iPhone} alt="vuesiri-iphone"/>
+                </div>
+                <div className="log-in-sign-up">
+                    <Link to="sign-in">login</Link>
+                    <Link to="sign-up">Sign Up</Link>
+                </div>
             </div>
         );
     }
